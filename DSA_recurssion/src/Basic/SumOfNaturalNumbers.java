f2026a98e8627dbd08efbd18e5b2783d1163e994
unknown <sagardahibhate1@gmail.com>
2024-05-26 16:23:18 +0530

    commit message

--- DSA_recurssion/src/Basic/SumOfNaturalNumbers.java
+++ DSA_recurssion/src/Basic/SumOfNaturalNumbers.java
@@ -2,4 +2,33 @@ package Basic;
 
 public class SumOfNaturalNumbers {
 
+	// using loop
+	public static int sumOfNatural_v1(int n) {
+		int sum = 0;
+		for (int i = 1; i <= n; i++) {
+			sum = sum + i;
+		}
+		return sum;
+	}
+
+	// using recursion
+	public static int sumOfNatural_v2(int n) {
+		if (n == 0)
+			return 0;
+		else
+			return n + sumOfNatural_v2(n - 1);
+	}
+
+	// using math formula
+	public static int sumOfNatural_v3(int n) {
+		return (n * (n + 1)) / 2;
+	}
+
+	public static void main(String[] args) {
+            System.out.println("Using loop : "+sumOfNatural_v1(5));
+            
+            System.out.println("Using recurssion :"+sumOfNatural_v2(4));
+            
+            System.out.println("using math formula :"+sumOfNatural_v3(3));
+	}
 }
