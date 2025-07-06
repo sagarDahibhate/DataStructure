828cf89a9aecdceaad4678fe2814a232d459d38f
SagarDahibhate <sagardahibhate1@gmail.com>
2025-07-06 11:14:42 +0530

    new files

---
+++ DSA_recurssion/src/com/sagar/elshad_dsa/Array/MiddleFunction.java
@@ -0,0 +1,23 @@
+package com.sagar.elshad_dsa.Array;
+
+import java.util.Arrays;
+
+public class MiddleFunction {
+
+	 public static int[] middleFunction(int [] arr) {
+		 int index=1;
+		 int [] myArray=new int[arr.length-2];
+		 
+		 while(index < arr.length-1) {
+			 myArray[index-1]=arr[index];
+		 }
+		 return myArray;
+	 }
+	
+	 public static void main(String[] args) {
+		int [] arr=new int[] {1,2,3,4,5,6};
+		System.out.println(Arrays.toString(middleFunction(arr)));
+		System.out.println("hello");
+		
+	}
+}
